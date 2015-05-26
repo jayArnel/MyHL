5b9063e3d86617031c2e849d92b02cd1e4b189be
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-27 04:50:11 +0800

    fixed error display

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,7 @@
                 $('#out').removeClass('bad');
             } catch(e) {
                 $('#out').addClass('bad');
-                $('#out').append(e);
-                console.log(e);
+                $('#out').append(e.verbose);
             }
             $('#out').append('<p class="gray">Done.</p>')
         });
@@ -101,7 +100,6 @@
                     } catch(e) {
                         $('#out').addClass('bad');
                         $('#out').append(e.verbose);
-                        console.log(e);
                     }
                     $('#out').append('<p class="gray">Done.</p>')
                 }
