9cc1225234b213e4c62e5d2b273da3ea79c0fa92
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-27 12:19:44 +0800

    cleaned functions in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,65 @@
-// requirejs.config({
-//     baseUrl: 'assets/js',
-// });
+$(document).ready(function(){
+	var openmenu = function() {
+		$('.menu').animate({
+          left: "0px"
+        }, 200);
 
-// require(['jquery', 'calc'], function($, calc) {
-    $(document).ready(function(){
+        $('.main-content').animate({
+          left: "225px"
+        }, 200);
 
-        $('.open-menu').click(function(){
-            $('.menu').animate({
-              left: "0px"
-            }, 200);
+        $('.icon-menu').hide();
+        $('.close-menu').show();
+	}
 
-            $('.main-content').animate({
-              left: "225px"
-            }, 200);
+	var closemenu = function(){
+		$('.menu').animate({
+          left: "-25px"
+        }, 200);
+        $('.main-content').animate({
+          left: "0px"
+        }, 200);
+        $('.close-menu').hide();
+        $('.open-menu').show();
+	}
+    $('.open-menu').click(openmenu);
 
-            $(this).hide();
-            $('.close-menu').show();
-        });
+    $('.close-menu').click(closemenu);
 
-        $('.close-menu').click(function(){
-            $('.menu').animate({
-              left: "-25px"
-            }, 200);
-            $('.main-content').animate({
-              left: "0px"
-            }, 200);
-            $(this).hide();
-            $('.open-menu').show();
-        });
-        var open_menu = function(){
-
+    $('#input').keyup(function() {
+        if($(this).val() != '') {
+            $(".run").removeAttr('disabled');
+        } else {
+            $(".run").attr('disabled','disabled');
         }
-        // $('.main-content').click(function() {
-        //     if($(this).css('left') == '225px') {
-        //         $('.menu').animate({
-        //           left: "-225px"
-        //         }, 200);
-        //         $(this).animate({
-        //           left: "0px"
-        //         }, 200);
-        //         $('.close-menu').hide();
-        //         $('.open-menu').show();
-        //     }
-        // });
-        $('#input').keyup(function() {
-            if($(this).val() != '') {
-                $(".run").removeAttr('disabled');
+     });
+    var run = function(){
+        variables = []
+        $('#out').html('<p class="gray">Run:</p>')
+        try{
+            parser.parse($('#input').val());
+            $('#out').removeClass('bad');
+        } catch(e) {
+            $('#out').addClass('bad');
+            $('#out').append(e.verbose);
+        }
+        $('#out').append('<p class="gray">Done.</p>')
+    }
+    $(".run").click(run);
+
+    $(document).keydown(function(e) {
+        if (e.ctrlKey && e.which == 77) {  // Ctrl + M
+            if($('.close-menu').is(":visible")) {
+                closemenu();
             } else {
-                $(".run").attr('disabled','disabled');
+                openmenu();
             }
-
-         });
-        $(".run").click(function(){
-            variables = []
-            $('#out').html('<p class="gray">Run:</p>')
-            try{
-                parser.parse($('#input').val());
-                $('#out').removeClass('bad');
-            } catch(e) {
-                $('#out').addClass('bad');
-                $('#out').append(e.verbose);
+        }
+        if (e.which == 113) { //F2
+            if ($('#input').val().trim().length > 0) {
+                run();
             }
-            $('#out').append('<p class="gray">Done.</p>')
-        });
-
-        $(document).keydown(function(e) {
-            console.log('key code is: ' + e.which + ' ' + (e.ctrlKey ? 'Ctrl' : '') + ' ' +
-                    (e.shiftKey ? 'Shift' : '') + ' ' + (e.altKey ? 'Alt' : ''));
-            if (e.ctrlKey && e.which == 77) { 
-                if($('.close-menu').is(":visible")) {
-                    $('.menu').animate({
-                      left: "-25px"
-                    }, 200);
-                    $('.main-content').animate({
-                      left: "0px"
-                    }, 200);
-                    $('.close-menu').hide();
-                    $('.open-menu').show();
-                } else {
-                    $('.menu').animate({
-                      left: "0px"
-                    }, 200);
-
-                    $('.main-content').animate({
-                      left: "225px"
-                    }, 200);
-
-                    $('.open-menu').hide();
-                    $('.close-menu').show();
-                }
-            }
-            if (e.which == 113) { 
-                if ($('#input').val().trim().length > 0) {
-                    variables = []
-                    $('#out').html('<p class="gray">Run:</p>')
-                    try{
-                        parser.parse($('#input').val());
-                        $('#out').removeClass('bad');
-                    } catch(e) {
-                        $('#out').addClass('bad');
-                        $('#out').append(e.verbose);
-                    }
-                    $('#out').append('<p class="gray">Done.</p>')
-                }
-            }
-        });
-
+        }
     });
-// });
+
+});
