2b66f6175d437d589009784d8eb9ac4dcf48cc1e
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-27 13:00:11 +0800

    removed action.js and added functions to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,5 +61,36 @@ $(document).ready(function(){
             }
         }
     });
+    var updateLines = function(data){
+        var numLines = data.lines.total,
+            house = document.getElementsByClassName('line-nums')[0],
+            html = '',
+            i;
+        for(i=0; i<numLines; i++){
+            html += '<div>'+(i+1)+'</div>';                 
+        }
+        house.innerHTML = html;
+    }
 
+    BehaveHooks.add(['keydown'], updateLines);
+    
+    var editor = new Behave({
+    
+        textarea:       document.getElementById('input'),
+        replaceTab:     true,
+        softTabs:       true,
+        tabSize:        4,
+        autoOpen:       true,
+        overwrite:      true,
+        autoStrip:      true,
+        autoIndent:     true,
+    });
+
+    $('#input').on('scroll', function () {
+        $('.line-nums').scrollTop($(this).scrollTop());
+    });
+
+    $('#input').bind('input propertychange', function() {
+        updateLines($(this).val());
+    });
 });
