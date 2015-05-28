3cfaf278080b94ed4d2bda5b8b5b058a41310ac0
Jay Arnel Bilocura <jay.arnel@gmail.com>
2015-05-29 03:44:27 +0800

    added comments on script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //function for opening sidebar menu
 	var openmenu = function() {
 		$('.menu').animate({
           left: "0px"
@@ -12,6 +14,7 @@ $(document).ready(function(){
         $('.close-menu').show();
 	}
 
+    //function for closing sidebar menu
 	var closemenu = function(){
 		$('.menu').animate({
           left: "-25px"
@@ -22,10 +25,14 @@ $(document).ready(function(){
         $('.close-menu').hide();
         $('.open-menu').show();
 	}
+
+    //open sidebar menu on click of icon menu
     $('.open-menu').click(openmenu);
 
+    //close sidebar menu on click of icon menu
     $('.close-menu').click(closemenu);
 
+    //disable run button on empty textarea
     $('#input').keyup(function() {
         if($(this).val() != '') {
             $(".run").removeAttr('disabled');
@@ -34,6 +41,7 @@ $(document).ready(function(){
         }
      });
 
+    //function for executing code
     var run = function(){
         variables = []
         $('#out').html('<span class="gray">Run:</span><br>')
@@ -47,25 +55,29 @@ $(document).ready(function(){
         $('#out').append('<p class="gray">Done.</p>')
     }
 
+    //run code on click of the Run button
     $(".run").click(run);
 
+    //keyboard shortcuts
     $(document).keyup(function(e) {
-        if (e.ctrlKey && e.which == 77) {  // Ctrl + M
+        if (e.ctrlKey && e.which == 77) {  // Ctrl + M, toggle sidebar menu
             if($('.close-menu').is(":visible")) {
                 closemenu();
             } else {
                 openmenu();
             }
-        } else if (e.which == 113) { //F2
+        } else if (e.which == 113) { //F2, run code
             if ($('#input').val().trim().length > 0) {
                 run();
             }else {
                 $("#out").text('');
             }
-        } else if (e.which == 27) {
+        } else if (e.which == 27) { //escape, clear output window
             $("#out").text('');
         }
     });
+
+    //behave.js code for textarea properties
     BehaveHooks.add(['keyup'], function(data){
         var numLines = data.lines.total,
             house = document.getElementsByClassName('line-nums')[0],
@@ -89,9 +101,8 @@ $(document).ready(function(){
         autoIndent:     true,
     });
 
+    //scroll line numbers and textarea together
     $('#input').on('scroll', function () {
         $('.line-nums').scrollTop($(this).scrollTop());
     });
-
-    //
 });
